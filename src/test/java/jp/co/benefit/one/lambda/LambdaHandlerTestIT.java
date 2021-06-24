28fe1f7ef2dc1b966f1f83010306a3b35c8a6632
quarkusio <no-reply@quarkus.io>
2021-06-24 05:19:28 +0000

    Initial commit

---
+++ src/test/java/jp/co/benefit/one/lambda/LambdaHandlerTestIT.java
@@ -0,0 +1,9 @@
+package jp.co.benefit.one.lambda;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class LambdaHandlerTestIT extends LambdaHandlerTest {
+
+    // Execute the same tests but in native mode.
+}
